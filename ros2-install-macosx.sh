b83f112db5ceb0f4aecd7a4575e1dac605f771d5
Anita Inchauspe <anitainchauspe@MacBook-Pro-de-Anita.local>
2015-08-31 18:00:21 +0200

    Installs ROS2

---
+++ ros2-install-macosx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Software License Agreement (BSD)
+#
+# Author    Anita Inchauspe <anita@erlerobotics.com>
+# Copyright (c) 2014-2015, Erle Robotics, Inc., All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+# the following conditions are met:
+# * Redistributions of source code must retain the above copyright notice, this list of conditions and the
+#   following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other materials provided with the distribution.
+# * Neither the name of Clearpath Robotics nor the names of its contributors may be used to endorse or
+#   promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+#For more info: https://github.com/ros2/ros2/wiki/OSX-Install-Binary
+
+#Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Install more stuff you need 
+brew tap osrf/ros2
+brew tap ros/deps
+brew install homebrew/science/opencv --without-python
+brew install python3 opensplice
+
+#Unpack the latest package for OS X
+mkdir -p ~/ros2_install
+cd ~/ros2_install
+
+#The name of the package may change in the future
+tar xf ~/Downloads/ros2-rc2-package-osx.tar.bz2
+
